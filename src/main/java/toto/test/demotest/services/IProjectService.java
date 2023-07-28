a49ce4955451bf89dac96383e5c05b44bb5a51f8
teuliere <teuliere.eric@imsa.msa.fr>
2023-07-28 09:17:05 +0200

    feat: demotest commit x

---
+++ src/main/java/toto/test/demotest/services/IProjectService.java
@@ -0,0 +1,12 @@
+package toto.test.demotest.services;
+
+import toto.test.demotest.models.Project;
+
+import java.util.List;
+
+public interface IProjectService {
+    List<Project> all();
+    Project byId(Long id);
+    Project saveOrUpdate(Project projet);
+    void deleteById(Long id);
+}
--- src/main/java/toto/test/demotest/services/ProjectService.java
+++ src/main/java/toto/test/demotest/services/ProjectService.java
@@ -1,7 +1,29 @@
 package toto.test.demotest.services;
 
 import org.springframework.stereotype.Service;
+import toto.test.demotest.models.Project;
+
+import java.util.List;
 
 @Service
-public class ProjectService {
+public class ProjectService implements IProjectService {
+    @Override
+    public List<Project> all() {
+        return null;
+    }
+
+    @Override
+    public Project byId(Long id) {
+        return null;
+    }
+
+    @Override
+    public Project saveOrUpdate(Project projet) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+    }
 }
