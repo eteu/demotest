334ecea7582325ddc52edb3a2ea2db2b57b4eb70
Yanis Adekalom <yanis971.60@gmail.com>
2023-07-28 14:43:46 +0200

    Feat(Routing): ProjectController

--- src/main/java/toto/test/demotest/controllers/ProjectController.java
+++ src/main/java/toto/test/demotest/controllers/ProjectController.java
@@ -1,14 +1,16 @@
 package toto.test.demotest.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import toto.test.demotest.models.Project;
 import toto.test.demotest.services.ProjectService;
 
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 @RestController
+@RequestMapping("/projects/")
 public class ProjectController {
     final private ProjectService service;
 
@@ -16,19 +18,24 @@ public class ProjectController {
         this.service = service;
     }
 
+    @GetMapping("all")
     public List<Project> all() {
         return service.all();
     }
 
-    public Project byId(Long id) {
+    @GetMapping("{id}")
+    public Project byId(@PathVariable Long id) {
         return service.byId(id);
     }
 
-    public Project saveOrUpdate(Project projet) {
-        return service.saveOrUpdate(projet);
+
+    @GetMapping("saveOrUpdate/{id}/{name}")
+    public Project saveOrUpdate(@PathVariable long id, @PathVariable String name) {
+        return service.saveOrUpdate(new Project(0L,name));
     }
 
-    public void deleteById(Long id) {
+    @GetMapping("delete/{id}")
+    public void deleteById(@PathVariable Long id) {
         service.deleteById(id);
     }
 }
