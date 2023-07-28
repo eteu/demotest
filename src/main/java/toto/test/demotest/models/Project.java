61555abf391d289d14f564f906da2af044594e49
teuliere <teuliere.eric@imsa.msa.fr>
2023-07-28 10:13:52 +0200

    feat: demotest commit x3

--- src/main/java/toto/test/demotest/models/Project.java
+++ src/main/java/toto/test/demotest/models/Project.java
@@ -1,12 +1,5 @@
 package toto.test.demotest.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Project {
     private long id;
     private String name;
