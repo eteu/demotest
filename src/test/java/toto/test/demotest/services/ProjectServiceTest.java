23d233b66266ee8a0bbfb4d193d0e9192dbb9f6f
Yanis Adekalom <yanis971.60@gmail.com>
2023-07-28 09:39:36 +0200

    Feat(Test): ProjectService
    Setter : ListeProject

--- src/test/java/toto/test/demotest/services/ProjectServiceTest.java
+++ src/test/java/toto/test/demotest/services/ProjectServiceTest.java
@@ -1,12 +1,28 @@
 package toto.test.demotest.services;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import toto.test.demotest.models.Project;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProjectServiceTest {
 
-    private ProjectService service = new ProjectService();
+    private final ProjectService service = new ProjectService();
+
+    @BeforeEach
+    public void setup() {
+        service.setListeProject(new ArrayList<>(List.of(new Project(), new Project())));
+    }
+
+    @AfterEach
+    public void clean() {
+        service.setListeProject(new ArrayList<>());
+    }
 
     @Test
     public void givenNothing_whenAll_ThenReturnInt() {
